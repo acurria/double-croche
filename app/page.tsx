9737596456bf3d924c1fb5f6f97a84829d5bd198
acurria <alexandrecurria@gmail.com>
2023-05-28 22:40:16 +0200

    main: image homepage

--- app/page.tsx
+++ app/page.tsx
@@ -43,67 +43,71 @@ export default function Home() {
 				<PreviewArticle/>
 			</div>
 			<div className="recent-articles-container bg-primary">
-				<h2>
-					<Fade left cascade>
-						<p className='highlight-secondary'>Derniers articles en </p>
-					</Fade>
-					<Fade left cascade>
-						date
-					</Fade>
-				</h2>
-				<div className='articles-wrapper lg:flex lg:items-start'>
-					<div className='main-article'>
-						<PreviewArticle/>
-					</div>
-					<div className='recent-articles-grid flex justify-center items-center flex-wrap lg:justify-end'>
-						<PreviewArticle/>
-						<PreviewArticle/>
-						<PreviewArticle/>
-						<PreviewArticle/>
-						<PreviewArticle/>
-						<PreviewArticle/>
-						<PreviewArticle/>
-						<PreviewArticle/>
+				<div className='recent-articles-wrapper lg:max-w-screen-2xl lg:mx-auto'>
+					<h2>
+						<Fade left cascade>
+							<p className='highlight-secondary'>Derniers articles en </p>
+						</Fade>
+						<Fade left cascade>
+							date
+						</Fade>
+					</h2>
+					<div className='articles-wrapper lg:flex lg:items-start'>
+						<div className='main-article'>
+							<PreviewArticle/>
+						</div>
+						<div className='recent-articles-grid flex justify-center items-center flex-wrap lg:justify-end'>
+							<PreviewArticle/>
+							<PreviewArticle/>
+							<PreviewArticle/>
+							<PreviewArticle/>
+							<PreviewArticle/>
+							<PreviewArticle/>
+							<PreviewArticle/>
+							<PreviewArticle/>
+						</div>
 					</div>
 				</div>
 			</div>
 			<div className="moment-container bg-white">
-				<h2 className='text-black'>
-					<Fade left cascade>
-						<p className='highlight-secondary'>En ce </p>
-					</Fade>
-					<Fade left cascade>
-						moment
-					</Fade>
-				</h2>
-				<div className='albums-films-wrapper lg:flex'>
-					<div className='albums-wrapper'>
-						<h3>Albums</h3>
-						<div className='albums-items lg:flex'>
-							<Slider {...settings} className="lg:hidden">
-								<Album/>
-								<Album/>
-								<Album/>
-							</Slider>
-							<div className="items-no-slideshow hidden lg:flex lg:w-full">
-								<Album/>
-								<Album/>
-								<Album/>
+				<div className='moment-wrapper lg:max-w-screen-2xl lg:mx-auto'>
+					<h2 className='text-black'>
+						<Fade left cascade>
+							<p className='highlight-secondary'>En ce </p>
+						</Fade>
+						<Fade left cascade>
+							moment
+						</Fade>
+					</h2>
+					<div className='albums-films-wrapper lg:flex'>
+						<div className='albums-wrapper'>
+							<h3>Albums</h3>
+							<div className='albums-items lg:flex'>
+								<Slider {...settings} className="lg:hidden">
+									<Album/>
+									<Album/>
+									<Album/>
+								</Slider>
+								<div className="items-no-slideshow hidden lg:flex lg:w-full">
+									<Album/>
+									<Album/>
+									<Album/>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div className='films-wrapper'>
-						<h3>Films</h3>
-						<div className='films-items lg:flex'>
-							<Slider {...settings} className="lg:hidden">
-								<Film/>
-								<Film/>
-								<Film/>
-							</Slider>
-							<div className="items-no-slideshow hidden lg:flex lg:w-full">
-								<Film/>
-								<Film/>
-								<Film/>
+						<div className='films-wrapper'>
+							<h3>Films</h3>
+							<div className='films-items lg:flex'>
+								<Slider {...settings} className="lg:hidden">
+									<Film/>
+									<Film/>
+									<Film/>
+								</Slider>
+								<div className="items-no-slideshow hidden lg:flex lg:w-full">
+									<Film/>
+									<Film/>
+									<Film/>
+								</div>
 							</div>
 						</div>
 					</div>
