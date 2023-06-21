ffd95fcd89b420b5105b688072c385632ac8dc46
Alexandre Curria <jimbra@dnd.fr>
2023-06-21 16:48:52 +0200

    main: add dynamic url

---
+++ app/articles/[article]/page.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import Image from 'next/image'
+import {useNextSanityImage} from 'next-sanity-image'
+// @ts-ignore
+import Fade from 'react-reveal/Fade'
+import {getArticle} from "@/src/createClient";
+import client from "@/src/createClient";
+import {PortableText} from '@portabletext/react'
+
+type propsType = {
+	params: {article: string}
+};
+
+export default async function Article({params}:propsType) {
+	const slug = params.article;
+	const article = await getArticle(slug);
+
+	const myPortableTextComponents = {
+		types: {
+			image: ({value}:any) => {
+				return (
+					<SanityImage {...value} />
+				);
+			},
+		},
+	};
+
+	const SanityImage = ({asset}:any) => {
+		const imageProps:any = useNextSanityImage(client, asset);
+
+		if (!imageProps) return null;
+
+		return (
+			<Image {...imageProps} alt="image-article"/>
+		);
+	}
+
+	return (
+		<main className="article bg-primary">
+			<div className="article-page">
+				<div className="header-article lg:mx-auto lg:max-w-screen-2xl">
+					<h1 className='sr-only'>{article.title}</h1>
+					<h2 className='title-mobile'>
+						<Fade left cascade>
+							<p className='highlight-secondary'>{article.category}</p>
+						</Fade>
+						<Fade left cascade>
+							{article.subcategory}
+						</Fade>
+					</h2>
+					<div className='main-section-article lg:flex'>
+						<Image
+							className="image-preview-article"
+							src={article.image}
+							alt="Vercel Logo"
+							width={500}
+							height={24}
+							priority
+						/>
+						<div className='wrapper-title'>
+							<h2 className='title-desktop'>
+								<Fade left cascade>
+									<p className='highlight-secondary'>{article.category}</p>
+								</Fade>
+								<Fade left cascade>
+									{article.subcategory}
+								</Fade>
+							</h2>
+							<p className='title-info'>
+								{
+									article.categorySlug === 'musique' && article.subcategorySlug === 'interview' && <span className='info'><span className='highlight-secondary'>{article.artist}</span>pour “{article.album}”</span>
+								}
+								{
+									article.categorySlug === 'musique' && article.subcategorySlug === 'festival' && <span className='info'><span className='highlight-secondary'>{article.musicFestivalName} - </span>{article.city} {article.year}</span>
+								}
+								{
+									article.categorySlug === 'cinema' && article.subcategorySlug === 'festival' && <span className='info'><span className='highlight-secondary'>{article.filmFestivalName} - </span>{article.year}</span>
+								}
+								{
+									article.categorySlug === 'cinema' && article.subcategorySlug === 'chronique' && <span className='info'><span className='highlight-secondary'>{article.filmTitle}</span> de {article.director}</span>
+								}
+								{
+									article.categorySlug === 'cinema' && article.subcategorySlug === 'interview' && <span className='info'><span className='highlight-secondary'>{article.director}</span> pour “{article.filmTitle}”</span>
+								}
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className='article-container'>
+				<div className='wrapper-article lg:max-w-screen-lg lg:mx-auto'>
+					<PortableText value={article.content} components={myPortableTextComponents}/>
+				</div>
+			</div>
+		</main>
+	)
+}
--- src/createClient.tsx
+++ src/createClient.tsx
@@ -1,5 +1,34 @@
 import {createClient} from "@sanity/client";
+import {groq} from "next-sanity";
 
 const client = createClient({projectId: '5ouijukq', dataset:'production', apiVersion:'2022-11-11', useCdn:true})
-
 export default client;
+
+export async function getArticle(slug: string) {
+    return client.fetch(
+        groq`*[_type=="articles" && slug.current == '${slug}'][0] {
+              "artist": artist->firstLastName,
+              "image": image.asset->url,
+              "album": album->title,
+              "category": category->title,
+              "categorySlug": category->slug.current,
+              "subcategory": subcategory->title,
+              "subcategorySlug": subcategory->slug.current,
+              "musicFestivalName": musicFestivalName->title,
+              "filmFestivalName": filmFestivalName->title,
+              "city": city,
+              "year": year,
+              "month": month,
+              "part": part,
+              "director": director->firstLastName,
+              "filmTitle": filmTitle->title,
+              "date": date,
+              "localisation": localisation,
+              "externalLink": link,
+              "url": slug.current,
+              "content": content,
+              "title": title
+        }`,
+        {slug}
+    )
+}
