84e92af976b07508a834cdad7473e8b16fb2521a
acurria <alexandrecurria@gmail.com>
2023-07-14 18:33:22 +0200

    main: article modify

--- app/articles/[article]/page.tsx
+++ app/articles/[article]/page.tsx
@@ -81,7 +81,7 @@ export default async function Article({params}:propsType) {
 									article.categorySlug === 'musique' && article.subcategorySlug === 'festival' && <span className='info'><span className='highlight-secondary'>{article.musicFestivalName} - </span>{article.city} {article.year}</span>
 								}
 								{
-									article.categorySlug === 'cinema' && article.subcategorySlug === 'festival' && <span className='info'><span className='highlight-secondary'>{article.filmFestivalName} - </span>{article.year} - {article.day}</span>
+									article.categorySlug === 'cinema' && article.subcategorySlug === 'festival' && <span className='info'><span className='highlight-secondary'>{article.filmFestivalName} - </span>{article.year}</span>
 								}
 								{
 									article.categorySlug === 'cinema' && article.subcategorySlug === 'chronique' && <span className='info'><span className='highlight-secondary'>{article.filmTitle} </span> de {article.director}</span>
