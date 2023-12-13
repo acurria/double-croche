54a76bd69d881a6f7b9c44b4a7f454b9e1e4ee99
Alexandre Curria <jimbra@dnd.fr>
2023-12-13 16:15:57 +0100

    evolution-2: Change structure page-main

--- app/recherche/page.tsx
+++ app/recherche/page.tsx
@@ -44,33 +44,35 @@ export default function Page() {
 
 	return (
 		<>
-			<div className="category-page bg-primary page-main search-results">
-				<div className="header-category lg:mx-auto lg:max-w-screen-2xl">
-					<h1>
-						<Fade left cascade>
-							<p className='highlight-secondary'>Résultats</p>
-						</Fade>
-						<Fade left cascade>
-							DE RECHERCHE
-						</Fade>
-					</h1>
-				</div>
-			</div>
-			<div id='grid-container' className='grid-container layout-basic'>
-				<div className='wrapper-grid lg:max-w-screen-2xl lg:mx-auto'>
-					{
-						data.articles.length === 0 && <div className='empty-search-container'>
-							<h2>Aucun résultat ne correspond à votre recherche.</h2>
-						</div>
-					}
-					{
-						data.articles.map(function (item:any, index:number){
-							return <Fade key={item._id} bottom>
-								<PreviewArticle key={item._id} id={item._id}/>
+			<div className="page-main">
+				<div className="category-page bg-primary search-results">
+					<div className="header-category lg:mx-auto lg:max-w-screen-2xl">
+						<h1>
+							<Fade left cascade>
+								<p className='highlight-secondary'>Résultats</p>
 							</Fade>
-							}
-						)
-					}
+							<Fade left cascade>
+								DE RECHERCHE
+							</Fade>
+						</h1>
+					</div>
+				</div>
+				<div id='grid-container' className='grid-container layout-basic'>
+					<div className='wrapper-grid lg:max-w-screen-2xl lg:mx-auto'>
+						{
+							data.articles.length === 0 && <div className='empty-search-container'>
+								<h2>Aucun résultat ne correspond à votre recherche.</h2>
+							</div>
+						}
+						{
+							data.articles.map(function (item:any, index:number){
+									return <Fade key={item._id} bottom>
+										<PreviewArticle key={item._id} id={item._id}/>
+									</Fade>
+								}
+							)
+						}
+					</div>
 				</div>
 			</div>
 			<Script async src="https://www.googletagmanager.com/gtag/js?id=UA-80564203-1" />
