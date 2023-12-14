06d3d003b09d96f531b979cc3240caea5cf75fa1
Alexandre Curria <jimbra@dnd.fr>
2023-12-14 16:31:11 +0100

    evolution-1: Add figure

--- app/articles/[article]/layout.tsx
+++ app/articles/[article]/layout.tsx
@@ -10,9 +10,27 @@ export async function generateMetadata({params}:propsType) {
 	const article = await getArticle(slug);
 
 	return {
-	  title: article.title,
-	  description: article.metadescription,
+		title: article.title,
+		description: article.metadescription,
+		openGraph: {
+			images: article.image,
+		}
 	}
+}
+
+export function generateImageMetadata() {
+	return [
+		{
+			contentType: 'image/png',
+			size: { width: 48, height: 48 },
+			id: 'small',
+		},
+		{
+			contentType: 'image/png',
+			size: { width: 72, height: 72 },
+			id: 'medium',
+		},
+	]
 }
 
 export default function RootLayout({
