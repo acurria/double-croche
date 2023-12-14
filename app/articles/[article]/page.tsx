eeb57912696fff4e8b47165d6ca493516bbdc15b
Alexandre Curria <jimbra@dnd.fr>
2023-12-14 15:47:11 +0100

    evolution-1: Add figure

--- app/articles/[article]/page.tsx
+++ app/articles/[article]/page.tsx
@@ -58,14 +58,11 @@ export default async function Article({params}:propsType) {
 						</Fade>
 					</h2>
 					<div className='main-section-article lg:flex'>
-						<Image
-							className="image-preview-article"
-							src={urlFor(article.image).url()}
-							alt="Vercel Logo"
-							width={800}
-							height={800}
-							data-was-processed="true"
-						/>
+						<picture>
+							<source srcSet={urlFor(article.image).url()} />
+							<img src={urlFor(article.image).url()}alt="" />
+						</picture>
+
 						<div className='wrapper-title'>
 							<h2 className='title-desktop'>
 								<Fade left cascade>
