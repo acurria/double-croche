b847a44c174f3283cb9adfc21aa902ec8aed1659
Alexandre Curria <jimbra@dnd.fr>
2023-12-14 16:32:41 +0100

    evolution-1: Add figure

--- app/articles/[article]/layout.tsx
+++ app/articles/[article]/layout.tsx
@@ -18,21 +18,6 @@ export async function generateMetadata({params}:propsType) {
 	}
 }
 
-export function generateImageMetadata() {
-	return [
-		{
-			contentType: 'image/png',
-			size: { width: 48, height: 48 },
-			id: 'small',
-		},
-		{
-			contentType: 'image/png',
-			size: { width: 72, height: 72 },
-			id: 'medium',
-		},
-	]
-}
-
 export default function RootLayout({
 	children,
 }: {
